f8a88274578561ae02c62a7a77f73da3907d2709
Zabodashka <igorsulga6@gmail.com>
2026-03-10 15:01:54 +0200

    Update user.py

--- services/user.py
+++ services/user.py
@@ -1,20 +1,24 @@
-from db.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 def create_user(
     username: str,
     password: str,
-    email: str,
-    first_name: str,
-    last_name: str
+    email: str = None,
+    first_name: str = None,
+    last_name: str = None
 ) -> User:
-    user = User.objects.create_user(
-        username=username,
-        password=password,
-        email=email,
-        first_name=first_name,
-        last_name=last_name
-    )
+    user = User(username=username)
+    user.set_password(password)
+    if email is not None:
+        user.email = email
+    if first_name is not None:
+        user.first_name = first_name
+    if last_name is not None:
+        user.last_name = last_name
+    user.save()
     return user
 
 
@@ -24,17 +28,22 @@ def get_user(user_id: int) -> User:
 
 def update_user(
     user_id: int,
-    username: str,
-    password: str,
-    email: str,
-    first_name: str,
-    last_name: str
+    username: str = None,
+    password: str = None,
+    email: str = None,
+    first_name: str = None,
+    last_name: str = None
 ) -> User:
-    user = User.objects.get(id=user_id)
-    user.username = username
-    user.set_password(password)
-    user.email = email
-    user.first_name = first_name
-    user.last_name = last_name
+    user = get_user(user_id)
+    if username is not None:
+        user.username = username
+    if password is not None:
+        user.set_password(password)
+    if email is not None:
+        user.email = email
+    if first_name is not None:
+        user.first_name = first_name
+    if last_name is not None:
+        user.last_name = last_name
     user.save()
     return user
