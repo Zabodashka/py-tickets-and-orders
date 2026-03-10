f35c61176fa115066e0953e97d77492665f2c40e
Zabodashka <igorsulga6@gmail.com>
2026-03-10 15:04:38 +0200

    Update movie.py

--- services/movie.py
+++ services/movie.py
@@ -1,14 +1,8 @@
-from db.models import Movie, Actor
-
-
-def create_movie(title: str, description: str, actors_ids: list[int]) -> Movie:
-    movie = Movie.objects.create(title=title, description=description)
-    movie.actors.set(actors_ids)
-    return movie
+from db.models import Movie
 
 
 def get_movies(title: str = None) -> list[Movie]:
-    qs = Movie.objects.all()
     if title:
-        qs = qs.filter(title__icontains=title)
-    return list(qs)
+        return list(Movie.objects.filter(title__icontains=title))
+    return list(Movie.objects.all())
+    
