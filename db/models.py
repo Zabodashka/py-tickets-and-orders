2025c7d3da2ad63caa153c5b3231abc7156ee39a
Zabodashka <igorsulga6@gmail.com>
2026-03-10 15:01:38 +0200

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
@@ -7,18 +7,48 @@ class User(AbstractUser):
 
 
 class Genre(models.Model):
-    name: str = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return self.name
 
 
 class Actor(models.Model):
-    name: str = models.CharField(max_length=255)
-    age: int = models.PositiveIntegerField(default=0)
-    genres: models.ManyToManyField = models.ManyToManyField(
-        Genre, related_name="actors"
-    )
+    name = models.CharField(max_length=255)
+    age = models.PositiveIntegerField(default=0)
+    genres = models.ManyToManyField(Genre, related_name="actors")
 
     def __str__(self) -> str:
         return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class MovieSession(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="movie_sessions")
+    show_time = models.DateTimeField()
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE, related_name="orders")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Ticket(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
